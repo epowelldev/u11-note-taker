11a1d0c6603b7c3052fa67806206b94d6a9b2e6a
Erickson Powell <ericksonpowell@gmail.com>
2020-12-20 20:51:57 -0800

    added delete functionality, updated to use unique ids

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 //Express Dependencies Setup
 var express = require("express");
@@ -26,13 +27,16 @@ app.get("/api/notes", function(req, res) {
 app.post("/api/notes", function(req, res) {
     //get `req.body` into a variable
     let newNote = req.body;
+    //add a unique id key:value to `newNote`
+    newNote.id = uuidv4();
+    // console.log(newNote);
+
     //get exsisting db.json file into object (return parsed file to let `dataObj`)
     fs.readFile("./db/db.json", (err, data) => {
         if (err) throw err;
         let dataObj = JSON.parse(data);
         //push `newNote` into `dataObj`
         dataObj.push(newNote);
-
         let updateDB = JSON.stringify(dataObj);
 
         //make the new `dataObj` into json and save to db.json
@@ -50,6 +54,34 @@ app.post("/api/notes", function(req, res) {
 });
 
 app.delete("/api/notes/:id", function(req, res) {
+    //get the db.json & parse to object
+    //access id from `req.params.id`
+
+    const noteToDelete = req.params.id;
+    console.log(noteToDelete);
+     
+
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        let dataObj = JSON.parse(data);
+        // console.log(dataObj);
+        //delete specific ID from array
+        let dataObj2 = dataObj.filter(element => element.id !== noteToDelete);
+        // console.log(dataObj2);
+
+        //make the new `dataObj` into json and save to db.json
+        let updateDB = JSON.stringify(dataObj2);
+        fs.writeFile("./db/db.json", updateDB, (err) => {
+            if (err) throw err;
+            console.log("note deleted, hopefully...")
+        });
+    });
+    //send updated note list back to client
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        let dataObj = JSON.parse(data);
+        res.json(dataObj);
+    });
 });
 
 //HTML routes!
